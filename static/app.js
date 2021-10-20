b280492d3d3068e211ca3ef0a0a28d75ddb3d7b0
dusty <dusty@yandex-team.ru>
2021-10-20 15:28:35 +0300

    recieve link to file on frontend

--- static/app.js
+++ static/app.js
@@ -4,5 +4,20 @@ const pond = FilePond.create({
   server: "/upload"
 });
 
+pond.on('processfile', (e, file) => {
+  const json = file.serverId;
+  const data = JSON.parse(json);
+
+  const id = data.id;
+
+  const link = document.createElement('a');
+
+  link.href = window.location + 'd/' + id;
+  link.textContent = link.href;
+
+  pond.element.remove();
+  document.body.appendChild(link);
+});
+
 // Add it to the DOM
 document.body.appendChild(pond.element);
